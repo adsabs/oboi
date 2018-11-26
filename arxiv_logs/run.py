e1ac425abe3d58d3378c6f27afb764e5ecaa7750
Roman Chyla <roman.chyla@gmail.com>
2018-11-26 17:54:36 -0500

    Forgotten to save

--- arxiv_logs/run.py
+++ arxiv_logs/run.py
@@ -1,3 +1,16 @@
+from arxiv_logs import app
+import sys
+
+def main(source_folder):
+    print 'Going to process: %s', source_folder
+    a = app.ArxivLogApplication('main')
+    a.process_folder(source_folder)
+    
 
 
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print 'Usage: run.py <source-folder>'
+        exit 1
+    main(sys.argv[1])
         
